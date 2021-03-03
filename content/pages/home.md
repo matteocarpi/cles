7f46d46ea699b9ce7fff64dc0721597d220f6cf8
Matteo Carpi <matteocarpi94@gmail.com>
2021-03-03 16:50:22 +0100

    Delete home

--- content/pages/home.md
+++
@@ -1,8 +0,0 @@
----
-title:
-  it: Casa
-  en: Home
-title_it: Casa
-title_en: Home
-
----
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,48 +1,54 @@
 /* eslint-disable no-undef */
-const path = require('path')
-const { createFilePath } = require('gatsby-source-filesystem')
+// Get your list of languages from somewhere, env file, config.json, etc
+// for sake of this snippet I am putting it here
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === 'MarkdownRemark') {
-    const longSlug = createFilePath({
-      node,
-      getNode,
-      basePath: 'content/pages',
-    })
-    const slug = longSlug.split('/')
-    createNodeField({
-      node,
-      name: 'slug',
-      value: `/${slug[slug.length - 2]}/`,
+const languages = ['it', 'en'] // English is currently the default so it isn't needed here.
+
+const createLocalePage = (page, createPage) => {
+  const { path, context, ...rest } = page
+
+  createPage({
+    ...rest,
+    context: {
+      ...context,
+      locale: defaultLanguage,
+    },
+  })
+
+  if (languages.length) {
+    languages.forEach(code => {
+      createPage({
+        ...rest,
+        path: `/${code}${path}`,
+        // every page for each language gets the language code as a prefix
+        // to its path: "/es/blog/<some-slug>" for example
+        context: {
+          ...context,
+          locale: code,
+        },
+      })
     })
   }
 }
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const result = await graphql(`
-    query {
-      allFile(filter: { relativeDirectory: { eq: "posts" } }) {
-        edges {
-          node {
-            childMarkdownRemark {
-              fields {
-                slug
-              }
-            }
-          }
-        }
-      }
-    }
+exports.createPages = ({ actions, graphql }) => {
+  const { data } = graphql(`
+    
   `)
-  result.data.allFile.edges.forEach(({ node }) => {
-    createPage({
-      path: node.childMarkdownRemark.fields.slug,
-      component: path.resolve('./src/templates/post.js'),
-      context: {
-        slug: node.childMarkdownRemark.fields.slug,
-      },
-    })
-  })
+  const { createPage } = actions
+  // generate your dynamic content here...
+  const page = {
+    path: 'some-page',
+    component: path.resolve(`./src/templates/some-page.js`),
+    context: {
+      slug: 'some-page-slug',
+    },
+  }
+  createLocalePage(page, createPage)
+}
+
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage, deletePage } = actions
+  deletePage(page)
+  createLocalePage(page, createPage)
 }
