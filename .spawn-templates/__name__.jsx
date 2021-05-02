aeac71c49695bcbcb292b75c7b5ba1f962e9e088
Matteo Carpi <matteocarpi94@gmail.com>
2021-05-02 14:24:10 +0200

    Sdfsldkj

---
+++ .spawn-templates/__name__.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function __name__({ children }) {
+  return (
+    <div>{ children }</div>
+  )
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -14,13 +18,21 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: `${__dirname}/src/assets`,
+        },
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `tameto-gatsby-wordpress`,
+        short_name: `tameto`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -30,6 +42,31 @@ module.exports = {
     },
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-styled-components`,
+    // {
+    //   resolve: 'gatsby-source-wordpress-experimental',
+    //   options: {
+    //     url: process.env.WORDPRESS_API_ENDPOINT,
+    //     // allows a fallback url if WPGRAPHQL_URL is not set in the env, this may be a local or remote WP instance.
+    //     schema: {
+    //       // Prefixes all WP Types with "Wp" so "Post and allPost" become "WpPost and allWpPost".
+    //       typePrefix: 'Wp',
+    //     },
+    //     develop: {
+    //       // caches media files outside of Gatsby's default cache an thus allows them to persist through a cache reset.
+    //       hardCacheMediaFiles: true,
+    //     },
+    //     type: {
+    //       Post: {
+    //         limit:
+    //           process.env.NODE_ENV === 'development'
+    //             ? // Lets just pull 50 posts in development to make it easy on ourselves (aka. faster).
+    //               20
+    //             : // and we don't actually need more than 5000 in production for this particular site
+    //               5000,
+    //       },
+    //     },
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
