e1c2fc0ff4aebf91ce2e9bb4d664ca4c58c219b2
Matteo Carpi <matteocarpi94@gmail.com>
2021-05-03 09:25:17 +0200

    Animated first level images

--- src/components/SlidingImages/SlidingImages.jsx
+++ src/components/SlidingImages/SlidingImages.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useRef, useLayoutEffect } from 'react'
 import styled from 'styled-components'
-import { motion } from 'framer-motion'
+import { motion, useAnimation } from 'framer-motion'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { graphql, useStaticQuery } from 'gatsby'
+
+import useElementInView from '../../hooks/useElementInView'
 
 const Container = styled.div`
   display: flex;
@@ -16,6 +18,34 @@ const GraphicContainer = styled(motion.div)`
 `
 
 const PhotoContainer = styled(motion.div)``
+
+const graphicVariants = {
+  hidden: {
+    translateX: 0,
+    opacity: 0,
+  },
+  visible: {
+    translateX: '30%',
+    opacity: 1,
+    transition: {
+      duration: 2,
+    },
+  },
+}
+
+const photoVariants = {
+  hidden: {
+    translateX: '30%',
+    opacity: 0,
+  },
+  visible: {
+    translateX: 0,
+    opacity: 1,
+    transition: {
+      duration: 2,
+    },
+  },
+}
 
 export default function SlidingImages() {
   const data = useStaticQuery(graphql`
@@ -37,15 +67,37 @@ export default function SlidingImages() {
     }
   `)
 
+  const ref = useRef()
+
+  const inView = useElementInView({ ref })
+
+  const controls = useAnimation()
+
+  useLayoutEffect(() => {
+    if (inView) {
+      controls.start('visible')
+    } else {
+      controls.start('hidden')
+    }
+  }, [controls, inView])
+
   const graphic = getImage(data.graphic.nodes[0])
   const photo = getImage(data.photo.nodes[0])
 
   return (
-    <Container>
-      <GraphicContainer>
+    <Container ref={ref}>
+      <GraphicContainer
+        variants={graphicVariants}
+        initial="hidden"
+        animate={controls}
+      >
         <GatsbyImage image={graphic} />
       </GraphicContainer>
-      <PhotoContainer>
+      <PhotoContainer
+        variants={photoVariants}
+        initial="hidden"
+        animate={controls}
+      >
         <GatsbyImage image={photo} />
       </PhotoContainer>
     </Container>
