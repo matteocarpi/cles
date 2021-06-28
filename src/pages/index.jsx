fee2a941b031a01d7668ea4da8d9b8c3bc781fef
Matteo Carpi <matteocarpi94@gmail.com>
2021-06-28 09:51:00 +0200

    Temp: no auto lang detection

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,25 @@
-import React, { useEffect } from 'react'
+import React from // , { useEffect }
+'react'
 
-import { navigate } from 'gatsby'
+// import { navigate } from 'gatsby'
 
 import Home from '../components/Home'
 
 const defaultLang = 'it'
 
-const IndexPage = () => {
-  const isBrowser = typeof window !== 'undefined'
+const IndexPage = () => (
+  // const isBrowser = typeof window !== 'undefined'
 
-  const locale = isBrowser && (navigator.language || navigator.userLanguage)
+  // const locale = isBrowser && (navigator.language || navigator.userLanguage)
 
-  const lang = isBrowser && locale.substring(0, 2)
+  // const lang = isBrowser && locale.substring(0, 2)
 
-  useEffect(
-    () => isBrowser && navigate(lang === defaultLang ? '/' : `/${lang}`),
-    [isBrowser, lang],
-  )
+  // useEffect(
+  //   () => isBrowser && navigate(lang === defaultLang ? '/' : `/${lang}`),
+  //   [isBrowser, lang],
+  // )
 
-  return <Home lang={defaultLang} />
-}
+  <Home lang={defaultLang} />
+)
 
 export default IndexPage
