b7927f51d54cd4826272959094238f4aa275e27b
Matteo Carpi <matteocarpi94@gmail.com>
2021-07-27 16:35:22 +0200

    style: appearing text for page intro

--- src/components/PageIntro/PageIntro.jsx
+++ src/components/PageIntro/PageIntro.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
+
+import useResponsiveness from '../../hooks/useResponsiveness'
 
 import SlidingImages from '../SlidingImages'
+import AppearingText from '../AppearingText'
 
 const Container = styled.section`
   margin-top: 124px;
@@ -15,15 +19,7 @@ const Container = styled.section`
   }
 `
 
-const Description = styled.h3`
-  padding: 0 20px;
-  max-width: 1000px;
-
-  @media (min-width: 768px) {
-    align-self: flex-start;
-    padding: 0;
-  }
-`
+const Description = styled(motion.h3)``
 
 const StyledSlidingImages = styled(SlidingImages)`
   align-self: flex-end;
@@ -33,10 +29,24 @@ const StyledSlidingImages = styled(SlidingImages)`
   }
 `
 
+const Text = styled(AppearingText)`
+  padding: 0 20px;
+  max-width: 1000px;
+
+  @media (min-width: 768px) {
+    align-self: flex-start;
+    padding: 0;
+  }
+`
+
 export default function PageIntro({ text, graphic, image }) {
+  const { isMobile } = useResponsiveness()
+
   return (
     <Container>
-      <Description>{text}</Description>
+      <Text maxStrLength={isMobile ? 25 : 40} component={Description}>
+        {text}
+      </Text>
       <StyledSlidingImages graphic={graphic} image={image} />
     </Container>
   )
