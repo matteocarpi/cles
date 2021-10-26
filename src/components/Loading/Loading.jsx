4bab1f37cbd71b5823980b7595c89035b32fe9b4
Matteo Carpi <matteocarpi94@gmail.com>
2021-10-26 17:00:08 +0200

    fix: logo

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
 
-import Logo from '../Logo'
+import Logo from '../../assets/logo-icon.svg'
 
 const spin = keyframes`
   from {
@@ -10,30 +10,20 @@ const spin = keyframes`
   to {
     transform: rotate(360deg);
   }
-  `
+`
 
 const Container = styled.div`
   position: relative;
   width: 80px;
   height: 50px;
   animation: ${spin} infinite 3s linear;
-`
-
-const StyledLogo = styled(Logo)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: transparent;
-  width: 100%;
-  height: 100%;
+  margin: 20px;
 `
 
 export default function Loading() {
   return (
     <Container>
-      <StyledLogo />
+      <Logo />
     </Container>
   )
 }
