07d3a68de3ae35ef002e3cd65b21c54e0940f3c5
Matteo Carpi <matteocarpi94@gmail.com>
2021-11-02 12:24:24 +0100

    feat: responsive logo swiper for clienti desktop

--- src/components/ClientSection/ClientSection.jsx
+++ src/components/ClientSection/ClientSection.jsx
@@ -9,6 +9,7 @@ import { v4 as uuidv4 } from 'uuid'
 import useLang from '../../hooks/useLang'
 
 import 'swiper/swiper.min.css'
+import useResponsiveness from '../../hooks/useResponsiveness'
 
 SwiperCore.use([Autoplay])
 
@@ -18,6 +19,11 @@ const Container = styled.article`
   border-bottom: solid 2px ${({ theme }) => theme.gray};
 `
 
+const SwiperWrapper = styled.div`
+  max-width: 900px;
+  margin-left: auto;
+`
+
 const Title = styled.h6`
   text-transform: uppercase;
   margin-bottom: 40px;
@@ -25,6 +31,12 @@ const Title = styled.h6`
 
 const Logo = styled(GatsbyImage)`
   width: 100%;
+
+  @media (min-width: 768px) {
+    width: 273px;
+    height: 117px;
+    margin-bottom: 15px;
+  }
 `
 
 const LogosContainer = styled.div`
@@ -37,63 +49,53 @@ const LogosContainer = styled.div`
 
 export default function ClientSection({ titolo, loghi = [] }) {
   const { lang } = useLang()
+  const { isMobile } = useResponsiveness()
 
   const logos = useMemo(
     () => loghi?.map(l => l.localFile.childImageSharp.gatsbyImageData) ?? [],
     [loghi],
   )
 
-  const slidesNumber = Math.ceil(logos.length / 3)
+  const rows = isMobile ? 3 : 2
+  const slidesNumber = Math.ceil(logos.length / rows)
 
   const slides = useMemo(
     () =>
       Array.from(Array(slidesNumber).keys()).map(slide => ({
         id: uuidv4(),
-        0: logos[slide * 3],
-        1: logos[slide * 3 + 1],
-        2: logos[slide * 3 + 2],
+        logos: Array.from(Array(rows).keys()).map(row => ({
+          ...logos[slide * rows + row],
+        })),
       })),
-    [logos, slidesNumber],
+    [logos, rows, slidesNumber],
   )
 
   return (
     <Container>
       <Title>{titolo[lang]}</Title>
 
-      <Swiper
-        slidesPerView={2}
-        spaceBetween={50}
-        autoplay={{ delay: 5000 }}
-        modules={[Autoplay]}
-      >
-        {slides.map(slide => (
-          <SwiperSlide key={slide.id}>
-            <LogosContainer>
-              {slide[0] && (
-                <Logo
-                  objectFit="contain"
-                  objectPosition="50% 50%"
-                  image={slide[0]}
-                />
-              )}
-              {slide[1] && (
-                <Logo
-                  objectFit="contain"
-                  objectPosition="50% 50%"
-                  image={slide[1]}
-                />
-              )}
-              {slide[2] && (
-                <Logo
-                  objectFit="contain"
-                  objectPosition="50% 50%"
-                  image={slide[2]}
-                />
-              )}
-            </LogosContainer>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <SwiperWrapper>
+        <Swiper
+          slidesPerView={isMobile ? 2 : 3}
+          spaceBetween={50}
+          autoplay={{ delay: 5000 }}
+          modules={[Autoplay]}
+        >
+          {slides.map(slide => (
+            <SwiperSlide key={slide.id}>
+              <LogosContainer>
+                {slide.logos.map(logo => (
+                  <Logo
+                    objectFit="contain"
+                    objectPosition="50% 50%"
+                    image={logo}
+                  />
+                ))}
+              </LogosContainer>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </SwiperWrapper>
     </Container>
   )
 }
